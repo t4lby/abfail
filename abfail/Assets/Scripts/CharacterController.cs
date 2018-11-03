6925799636e69069d80805b2117e6ba6110ead88
t4lby <alextalbot2@hotmail.co.uk>
2018-11-03 15:17:28 +0000

    progress

---
+++ abfail/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public List<Rigidbody2D> CharacterRbs;
+	
+    private bool charStoppedFlag;
+
+    private void Start()
+    {
+        charStoppedFlag = false;
+    }
+
+    private void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space) && !charStoppedFlag)
+        {
+            foreach (var rb in CharacterRbs)
+            {
+                rb.velocity = Vector2.zero;
+            }
+            charStoppedFlag = true;
+        }
+	}
+
+    public void PlatformHit()
+    {
+        print("woiii");
+    }
+}
---
+++ abfail/Assets/Scripts/FootDetector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootDetector : MonoBehaviour {
+
+    public CharacterController Character;
+
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if (other.CompareTag("foot"))
+        {
+            Character.PlatformHit();
+        }
+    }
+}
